70e81d069c7ccbc0e905721c06985e4e4975a169
ofeist <ofeist@gmail.com>
2024-12-08 13:33:16 +0100

    initial

---
+++ eck-elk/kube-vip-solution/create-pv-dirs.sh
@@ -0,0 +1,4 @@
+sudo mkdir -p /mnt/data/elasticsearch/data-0
+sudo chown -R 1000:1000 /mnt/data/elasticsearch/data-0
+sudo chmod -R 750 /mnt/data/elasticsearch/data-0
+
---
+++ eck-elk/kube-vip-solution/readme.md
@@ -0,0 +1,2 @@
+-- turn off kibana-ingress.yaml and elasticsearch-ingress.yaml first
+
